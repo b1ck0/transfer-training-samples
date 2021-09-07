2fd2549bc4bac638868bdc82a41a2c87a128c4bd
Vasil Yordanov <vyord@softserveinc.com>
2021-09-07 16:07:24 +0300

    tf_config bugfix 4

--- tf-mnist-efficientnetb0-multi-node/run_worker.sh
+++ tf-mnist-efficientnetb0-multi-node/run_worker.sh
@@ -1,2 +1,2 @@
 sudo docker build -t tf-mnist-multi-node .
-sudo docker run --gpus all -it tf-mnist-multi-node --env ROLE=worker --network=host -p 2222:2222
+sudo docker run --gpus all -it tf-mnist-multi-node --env ROLE='worker' --network=host -p 2222:2222
