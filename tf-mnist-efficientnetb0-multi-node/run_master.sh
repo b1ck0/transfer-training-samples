df9c71cf3258f97890fc74f4fb92488ff07146b6
Vasil Yordanov <vyord@softserveinc.com>
2021-09-07 15:51:43 +0300

    expose host network

--- tf-mnist-efficientnetb0-multi-node/run_master.sh
+++ tf-mnist-efficientnetb0-multi-node/run_master.sh
@@ -1,2 +1,2 @@
 sudo docker build -t tf-mnist-multi-node .
-sudo docker run --gpus all -it tf-mnist-multi-node --env ROLE=master
+sudo docker run --gpus all -it tf-mnist-multi-node --env ROLE=master --network=host
