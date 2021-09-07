488c66868ba1d5f5ba497b1b90df4c9967707511
Vasil Yordanov <vyord@softserveinc.com>
2021-09-07 15:52:33 +0300

    port-forwarding

--- tf-mnist-efficientnetb0-multi-node/run_master.sh
+++ tf-mnist-efficientnetb0-multi-node/run_master.sh
@@ -1,2 +1,2 @@
 sudo docker build -t tf-mnist-multi-node .
-sudo docker run --gpus all -it tf-mnist-multi-node --env ROLE=master --network=host
+sudo docker run --gpus all -it tf-mnist-multi-node --env ROLE=master --network=host -p 2222:2222
